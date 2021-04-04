082d9f6dac273e6fd7c94d7538722df27669e10d
Bian2017 <bgl_cumt_zju@163.com>
2021-04-04 22:02:29 +0800

    ✨feat: 测试import-local第三方依赖

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,3 +1,9 @@
 #! /usr/bin/env node
 
-console.log('hello, ii-cli!!');
+const importLocal = require('import-local');
+
+if (importLocal(__filename)) {
+  require('npmlog').info('cli', "is running ii-cli local's version");
+} else {
+  require('../lib')(process.argv.slice(2));
+}
