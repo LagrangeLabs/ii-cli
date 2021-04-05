5c7f85e84e8a4ec64e37f83402a3bd6ff2b82283
Bian2017 <bgl_cumt_zju@163.com>
2021-04-05 10:23:41 +0800

    ✨feat: 完成Node版本号的添加

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,14 +1,30 @@
 'use strict';
 
-module.exports = core;
-
+const semver = require('semver');
+const colors = require('colors/safe');
 const pkg = require('../package.json');
 const log = require('@ii-cli/log');
+const { LOWEST_NODE_VERSION } = require('./const');
 
 function core() {
-  checkPkgVer();
+  try {
+    checkPkgVer();
+    checkNodeVer();
+  } catch (e) {
+    log.error(e.message);
+  }
 }
 
 function checkPkgVer() {
   log.info('cli', pkg.version);
 }
+
+// 避免部分 Node API 在低版本时不支持
+function checkNodeVer() {
+  const currentVer = process.version;
+  if (!semver.gte(currentVer, LOWEST_NODE_VERSION)) {
+    throw new Error(colors.red(`ii-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的Node.js`));
+  }
+}
+
+module.exports = core;
--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -3,7 +3,7 @@
 const log = require('npmlog');
 
 // 判断是否开启Debug模式
-log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'success';
+log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
 
 // 修改前缀
 log.heading = 'ii';
