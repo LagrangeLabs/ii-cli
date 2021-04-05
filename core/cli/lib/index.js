5215f76c35f49d5f752259823055640c0a53750c
Bian2017 <bgl_cumt_zju@163.com>
2021-04-05 10:45:47 +0800

    ✨feat: 检查root账户权限

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -10,6 +10,7 @@ function core() {
   try {
     checkPkgVer();
     checkNodeVer();
+    checkRoot();
   } catch (e) {
     log.error(e.message);
   }
@@ -25,6 +26,19 @@ function checkNodeVer() {
   if (!semver.gte(currentVer, LOWEST_NODE_VERSION)) {
     throw new Error(colors.red(`ii-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的Node.js`));
   }
+}
+
+/**
+ * 检查 root 账户
+ *
+ * + 避免 root 账户创建的文件无法进行修改
+ * + 通过 root-check 对用户进行了降级
+ */
+function checkRoot() {
+  // root-check 利用 process.getuid API 可以获取当前用户登录的 id(默认是 501)。如果通过 sudo 启动，则是0(即超级管理员，root账户)
+  // root-check 利用 process.setuid/process.setgid API 来修改用户登录的id
+  const rootCheck = require('root-check');
+  rootCheck();
 }
 
 module.exports = core;
