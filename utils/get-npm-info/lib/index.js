be37b6d3af3d016fc190b00170860800e5f291be
Bian2017 <bgl_cumt_zju@163.com>
2021-04-18 21:59:17 +0800

    ✨feat: 获取NPM最新版本号功能

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -1,14 +1,49 @@
 'use strict';
 
-function getNpmInfo() {
-  // TODO
+const axios = require('axios');
+const urlJoin = require('url-join');
+const semver = require('semver');
+
+// 从 registry 中获取 npm 的信息
+function getNpmInfo(npm, registry) {
+  const register = registry || getDftRegistry();
+  const url = urlJoin(register, npm);
+
+  return axios.get(url).then(function (response) {
+    try {
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  });
+}
+
+async function getNpmVersions(npmName, registry) {
+  const data = await getNpmInfo(npmName, registry);
+  if (data) {
+    return Object.keys(data.versions);
+  } else {
+    return [];
+  }
 }
 
 function getDftRegistry(isOriginal = false) {
   return isOriginal ? 'https://registry.npmjs.org' : 'https://registry.npm.taobao.org';
 }
 
+async function getNpmLatestVersion(npmName, registry) {
+  let versions = await getNpmVersions(npmName, registry);
+  if (versions) {
+    return versions.sort((a, b) => semver.gt(b, a))[0];
+  }
+
+  return null;
+}
+
 module.exports = {
   getNpmInfo,
   getDftRegistry,
+  getNpmLatestVersion,
 };
